d9ab7b0ae01f02096701f537edabcee8c041f506
Liangyu Wu <133572075+Liangyu5wu@users.noreply.github.com>
2025-06-14 00:29:15 -0700

    Update processmu200_jetmatching_reco.C

--- reco/reconstruction/processmu200_jetmatching_reco.C
+++ reco/reconstruction/processmu200_jetmatching_reco.C
@@ -511,7 +511,7 @@ void processmu200_jetmatching_reco(float energyThreshold = 1.0, int startIndex =
     unmatchedVertices = 0;
     initialize_histograms();
 
-    const std::string path = "../SuperNtuple_mu200";
+    const std::string path = "./SuperNtuple_mu200";
     for (int i = startIndex; i <= endIndex; ++i) {
         std::ostringstream filename;
         filename << path << "/user.scheong.43348828.Output._" 
