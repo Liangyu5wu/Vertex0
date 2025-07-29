3d2d69f601c650b2fca77a6286406cf8216e2b03
Liangyu Wu <133572075+Liangyu5wu@users.noreply.github.com>
2025-07-29 11:28:01 -0700

    Create __init__.py

---
+++ ml_algr/config/__init__.py
@@ -0,0 +1,6 @@
+"""Configuration module for vertex time prediction models."""
+
+from .base_config import BaseConfig
+from .transformer_config import TransformerConfig
+
+__all__ = ['BaseConfig', 'TransformerConfig']
